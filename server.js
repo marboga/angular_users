1e24c6a136ee915635d149ad59daf4c56081fd94
Michael Arbogast <marboga@gmail.com>
2016-04-12 19:49:50 -0700

    table implemented, basic html

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + "static"));
+
+app.get('/', function(req, res) {
+	res.render('users')
+})
 
 app.listen(8000, function(){
 	console.log('listening on 8k');
